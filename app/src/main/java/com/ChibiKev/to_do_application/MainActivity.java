c3469f93d7ed97811aea7499720d4400aac1d11e
ChibiKev <k3v.ch3n@gmail.com>
2020-12-19 14:57:34 -0500

    Successfully Able to Add Items

--- app/src/main/java/com/ChibiKev/to_do_application/MainActivity.java
+++ app/src/main/java/com/ChibiKev/to_do_application/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,11 +30,22 @@ public class MainActivity extends AppCompatActivity {
         showItems = findViewById(R.id.showItems);
 
         items = new ArrayList<>();
-        items.add("Hello");
-        items.add("test");
 
         ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         showItems.setAdapter(itemsAdapter);
         showItems.setLayoutManager(new LinearLayoutManager(this));
+
+        toDoAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toDoItem = textItem.getText().toString();
+                // Add Item to Model
+                items.add(toDoItem);
+                // Modify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                textItem.setText("");
+                Toast.makeText(getApplicationContext(),"Item Successfully Added",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
